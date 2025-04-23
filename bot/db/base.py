8b733d720ee7ce92570d0024cee7ad29ac7eea32
Mifrain <62434656+Mifrain@users.noreply.github.com>
2025-04-23 12:34:02 +0300

    Update

--- bot/db/base.py
+++ bot/db/base.py
@@ -6,18 +6,7 @@ from bot.config import settings
 
 
 engine = create_engine(settings.DB_URL)
-# engine = create_engine("sqlite:///sites.db")
 
 sync_session = sessionmaker(bind=engine)
 
 Base = declarative_base()
-
-
-# Пример использования
-# if __name__ == "__main__":
-#     db = SessionLocal()
-#     sites = read_excel_to_models(Path("sites.xlsx"))
-#     for site in sites:
-#         create_site(db, site)
-#
-#     print(get_all_sites(db))
